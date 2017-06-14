7ccd8518c25bdeb2ce5b0ce0a566090a116e5c67
Jerry <jerry.tamlander@helsinki.fi>
2017-06-14 20:17:38 +0300

    Fixed search feature

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
 $(document).ready(function() {
     
-    var query = $('#searchquery').val();
+    var results = [];
     
-  
-    $(".random-button").on('click', function() {
-        window.alert(query);
-        //window.open('https://en.wikipedia.org/wiki/Special:Random');
+    
+    var api = 'https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=';
+    var cb = '&callback=JSON_CALLBACK';
+    
+    
+    document.getElementById("searchquery")
+        .addEventListener("keyup", function(event){
+        event.preventDefault();
+        if(event.keyCode == 13) {
+            let query = $('#searchquery').val();
+            console.log($('#searchquery').val());
+        }
     });
+    
+    
+    
+    $(".random-button").on('click', function() {
+        window.open('https://en.wikipedia.org/wiki/Special:Random');
+    });
+    
 });
